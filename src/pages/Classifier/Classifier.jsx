1c9aab45045aa4e6eb0715f30d4c704af87c7da2
Kadhir Umasankar <kadhirus99@utexas.edu>
2019-11-22 16:02:27 -0600

    Classifier: Added image cropper

--- src/pages/Classifier/Classifier.jsx
+++ src/pages/Classifier/Classifier.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { ImageContext } from '../../contexts/ImageContext'
 import {Container, Header, Grid, Form, Card, Button} from 'semantic-ui-react';
+import ReactCrop from 'react-image-crop';
+
+import 'react-image-crop/dist/ReactCrop.css';
+
+import fillerImg from './../imagery/uava_logo.png'
 
 function importAll(r) {
   return r.keys().map(r);
@@ -59,7 +64,10 @@ export default class Classifier extends React.Component {
       shape: '',
       color: '',
       alphanumeric: '',
-      alphanumeric_color: ''
+      alphanumeric_color: '',
+      crop: {
+        aspect: 1,
+      },
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -92,21 +100,110 @@ export default class Classifier extends React.Component {
     })
   }
 
+  onImageLoaded = image => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = (crop, percentCrop) => {
+    console.log('onCropComplete', crop, percentCrop);
+    this.makeClientCrop(crop);
+  };
+
+  onCropChange = (crop, percentCrop) => {
+    // console.log('onCropChange', crop, percentCrop);
+    // this.setState({ crop: percentCrop });
+    this.setState({ crop });
+  };
+
+  onDragStart = () => {
+    console.log('onDragStart');
+  };
+
+  onDragEnd = () => {
+    console.log('onDragEnd');
+  };
+
+  onChangeToIncompleteCropClick = () => {
+    this.setState({
+      crop: {
+        aspect: 16 / 9,
+        unit: '%',
+        width: 100,
+      },
+    });
+  };
+
+  getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise(resolve => {
+      canvas.toBlob(blob => {
+        blob.name = fileName; // eslint-disable-line no-param-reassign
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, 'image/jpeg');
+    });
+  }
+
+  makeClientCrop(crop) {
+      if (this.imageRef && crop.width && crop.height) {
+        this.getCroppedImg(this.imageRef, crop, 'newFile.jpeg').then(croppedImageUrl =>
+          this.setState({ croppedImageUrl })
+        );
+      }
+    }
+
   render() {
     const { currentImageIndex, changeImageIndex } = this.context
-    const {shape, color, alphanumeric, alphanumeric_color} = this.state
+    const {shape, color, alphanumeric, alphanumeric_color, crop, croppedImageUrl} = this.state
+    const src = imageObjects[currentImageIndex].image
 
     return (<Container id='page'>
       <Header as='h1'>Classifier</Header>
       <Grid>
         <Grid.Column width={11}>
-          <Card fluid image={imageObjects[currentImageIndex].image} description={imageObjects[currentImageIndex].name} />
+          {src && (
+            <ReactCrop
+              src={src}
+              crop={crop}
+              onImageLoaded={this.onImageLoaded}
+              onComplete={this.onCropComplete}
+              onChange={this.onCropChange}
+              onDragStart={this.onDragStart}
+              onDragEnd={this.onDragEnd}
+            />
+          )}
           <Card.Content extra style={{display:'flex', justifyContent: 'space-between'}}>
             <Button name='left-button' icon='arrow left' onClick={() => changeImageIndex(currentImageIndex-1)} disabled={currentImageIndex===0}/>
             <Button name='right-button' icon='arrow right' onClick={() => changeImageIndex(currentImageIndex+1)} disabled={currentImageIndex===imageObjects.length-1}/>
           </Card.Content>
         </Grid.Column>
         <Grid.Column width={5}>
+          <Container fluid textAlign='center'>
+            {(
+              <img alt="Crop" style={{ width: '300px' }} src={croppedImageUrl ? croppedImageUrl : fillerImg} />
+            )}
+            <br />
+            <br />
+          </Container>
           <Form>
             <Form.Group widths="equal">
               <Form.Dropdown placeholder='Select Shape' search fluid name='shape' selection value={shape} options={shapeOptions} onChange={this.handleChange}/>
